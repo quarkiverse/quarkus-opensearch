a68148d1713939812565e2887398f5fc2538c5a9
Loïc Mathieu <loikeseke@gmail.com>
2022-12-28 12:00:25 +0100

    Remove distribution as this extension will only support OpenSearch

--- opensearch-rest-client/deployment/src/main/java/io/quarkiverse/opensearch/restclient/lowlevel/deployment/DevservicesOpenSearchBuildItem.java
+++ opensearch-rest-client/deployment/src/main/java/io/quarkiverse/opensearch/restclient/lowlevel/deployment/DevservicesOpenSearchBuildItem.java
@@ -6,18 +6,15 @@ public final class DevservicesOpenSearchBuildItem extends MultiBuildItem {
     private final String hostsConfigProperty;
 
     private final String version;
-    private final Distribution distribution;
 
     public DevservicesOpenSearchBuildItem(String hostsConfigProperty) {
         this.hostsConfigProperty = hostsConfigProperty;
         this.version = null;
-        this.distribution = Distribution.OPENSEARCH;
     }
 
-    public DevservicesOpenSearchBuildItem(String configItemName, String version, Distribution distribution) {
+    public DevservicesOpenSearchBuildItem(String configItemName, String version) {
         this.hostsConfigProperty = configItemName;
         this.version = version;
-        this.distribution = distribution;
     }
 
     public String getHostsConfigProperty() {
@@ -28,12 +25,4 @@ public final class DevservicesOpenSearchBuildItem extends MultiBuildItem {
         return version;
     }
 
-    public Distribution getDistribution() {
-        return distribution;
-    }
-
-    public enum Distribution {
-        ELASTIC,
-        OPENSEARCH
-    }
 }

fd19253be90947fc448b042e044a4d307c1be832
Sven Boeckelmann <sboeckelmann@benelog.com>
2024-07-16 19:47:04 +0200

    improve example and native support

--- examples/quarkus-opensearch-simple-example/src/main/java/io/quarkiverse/opensearch/IndicesResource.java
+++ examples/quarkus-opensearch-simple-example/src/main/java/io/quarkiverse/opensearch/IndicesResource.java
@@ -9,17 +9,33 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import org.opensearch.client.opensearch.OpenSearchAsyncClient;
 import org.opensearch.client.opensearch.OpenSearchClient;
 
-@Path("/indices")
+import io.smallrye.mutiny.Uni;
+
+@Path("/")
 public class IndicesResource {
 
     @Inject
     OpenSearchClient client;
 
+    @Inject
+    OpenSearchAsyncClient asyncClient;
+
     @GET
+    @Path("/indices")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() throws IOException {
+    public String indices() throws IOException {
         return client.cat().indices().valueBody().stream().map(e -> e.index()).collect(Collectors.joining("\n"));
+    }
+
+    @GET
+    @Path("/indices-async")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Uni<String> indicesAsync() throws IOException {
+        return Uni.createFrom().completionStage(asyncClient.cat().indices())
+                .chain(r -> Uni.createFrom()
+                        .item(r.valueBody().stream().map(t -> t.index()).collect(Collectors.joining("\n"))));
     }
 }
